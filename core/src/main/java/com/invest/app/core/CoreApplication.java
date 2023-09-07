c36f7b9e5f9c49d68bae691e9606c9184a0307ad
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-09-07 22:44:25 +0300

    I added UserController class, that uses all methods, defined in UserService class from user_operator module.

--- core/src/main/java/com/invest/app/core/CoreApplication.java
+++ core/src/main/java/com/invest/app/core/CoreApplication.java
@@ -1,11 +1,16 @@
 package com.invest.app.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.invest.app.user_operator.repository.UserRepository;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.invest.app", "com.invest.app.user_operator"})
 public class CoreApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
---
+++ core/src/main/java/com/invest/app/core/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.invest.app.core.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.invest.app.user_operator.model.User;
+import com.invest.app.user_operator.repository.UserService;
+import com.invest.app.user_operator.repository.UserRepository;
+
+@CrossOrigin(originPatterns = "http://localhost:4200/")
+@RequestMapping("/investapp.com/user")
+@RestController
+public class UserController {
+	
+	@Autowired
+	public UserService service;
+	
+	@GetMapping("/")
+	public User getUserById(@RequestParam Long id) {
+		
+		return service.getUserById(id);
+	}
+	
+	@GetMapping("/all") 
+	public List<User> getAllUsers () {
+		
+		return service.getAllUsers();
+	}
+	
+	@PostMapping("/save")
+	public User saveUser(@RequestBody User user) {
+		
+		return service.saveUser(user);
+	}
+	 
+	@DeleteMapping("/delete") 
+	public void  deleteUser(@RequestParam Long id) {
+		
+		service.deleteUser(id);
+	}
+}
