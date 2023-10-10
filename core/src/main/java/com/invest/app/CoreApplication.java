e365a6b8c3b74b44f90235794e6e386ceb073d68
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-10-11 00:43:15 +0300

    Fixed previous commit bug.

--- core/src/main/java/com/invest/app/CoreApplication.java
+++ core/src/main/java/com/invest/app/CoreApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.invest.app.user_operator.repository.UserRepository;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.invest.app.core", "com.invest.app.user_operator", "com.invest.app.data_extract"})
-@EnableJpaRepositories(basePackages = "com.invest.app.user_operator.repository")
+@ComponentScan(basePackages = {"com.invest.app.core", "com.invest.app.user_operator"})
+@EnableJpaRepositories(basePackages = {"com.invest.app.user_operator.repository"})
 public class CoreApplication {
 	
 	public static void main(String[] args) {
